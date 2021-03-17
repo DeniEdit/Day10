1d4e988b138d673789915228b583415b60b6bc4e
DeniEdit <milicevic.deni@gmail.com>
2021-03-17 20:29:01 +0100

    Exercise 09

---
+++ ex09/src/index.js
@@ -0,0 +1,13 @@
+function myFunction(myObj, checkProp) {
+    // Only change code below this line
+    var myObj = {
+        title: "Titanic",
+        song: "My Heart Will Go On",
+        genre: "drama"
+    };
+    
+    return "Not Found";
+    // Only change code below this line
+}
+console.log(myFunction());
+module.exports = myFunction;
