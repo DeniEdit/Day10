4b0b62a719a64fe4d4b72cbc50aecffb53cb493b
DeniEdit <milicevic.deni@gmail.com>
2021-03-17 23:02:37 +0100

    Exercise 09

--- ex09/src/index.js
+++ ex09/src/index.js
@@ -1,16 +1,12 @@
 function myFunction(myObj, checkProp) {
     // Only change code below this line
-    var myObj = {
-        title: "Titanic",
-        song: "My Heart Will Go On",
-        genre: "drama"
-    };
     if (myObj.hasOwnProperty(checkProp)) {
-        return myObj[checkProp];
-      } else {
-        return "Not Found";
+      return myObj[checkProp];
+    } else {
+      return "Not Found";
+      // Only change code above this line
     }
-    // Only change code above this line
-}
-console.log(myFunction());
-module.exports = myFunction;
+  }
+  myFunction();
+  console.log(myFunction({ title: "Titanic", song: "My Heart Will Go On", genre: "drama" },"title"));
+  module.exports = myFunction;
