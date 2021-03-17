6f8383d19969c212947e3deb84d98412af6e10cf
DeniEdit <milicevic.deni@gmail.com>
2021-03-17 17:11:41 +0100

    Exercise 08

--- ex08/src/index.js
+++ ex08/src/index.js
@@ -1,35 +1,23 @@
 function getUsername(email) {
-    var username = '';
-
-    // Only change code below this line
-    switch (email) {
-        case 'user1@mail.com':
-            username = 'user1';
-            break;
-        case 'user2@mail.com':
-            username = 'user2';
-            break;
-        case 'user3@mail.com':
-            username = 'user3';
-            break;
-        case 'user4@mail.com':
-            username = 'user4';
-            break;
-        case 'user5@mail.com':
-            username = 'user5';
-            break;
-        case 'user6@mail.com':
-            username = 'user6';
-            break;
-        default:
-            username = 'undefined';
-            break;
-    }
-    // Only change code above this line
+    var username = {
+        "user1@mail.com" : "user1",
+        "user2@mail.com" : "user2",
+        "user3@mail.com" : "user3",
+        "user4@mail.com" : "user4",
+        "user5@mail.com" : "user5",
+        "user6@mail.com" : "user6",
+    };
+    var username = username[email];
 
     return username;
 }
-
+getUsername("user1@mail.com");
+getUsername("user2@mail.com");
+getUsername("user3@mail.com");
+getUsername("user4@mail.com");
+getUsername("user5@mail.com");
+getUsername("user6@mail.com");
+getUsername("");
 console.log(getUsername("user1@mail.com"));
 console.log(getUsername("user2@mail.com"));
 console.log(getUsername("user3@mail.com"));
