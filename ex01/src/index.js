13dc6611a3ddccf982ad5dbeffd666326bd50459
DeniEdit <milicevic.deni@gmail.com>
2021-03-17 12:20:45 +0100

    Exercise 1

---
+++ ex01/src/index.js
@@ -0,0 +1,13 @@
+// Only change code below this line
+var myPet = {
+    species: "Retriver",
+    name: "Balto",
+    legs: 4,
+    friends: ["Kan" , "Mik"]
+}
+function myFunction(myObj) {
+    return myObj;
+}
+// Only change code below this line
+console.log(myFunction(myPet)); // Change this line
+module.exports = { myPet, myFunction };
